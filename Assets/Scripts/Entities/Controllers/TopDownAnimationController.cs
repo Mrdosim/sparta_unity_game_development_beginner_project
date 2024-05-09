8e050ab8e4aaca885be4327415586f797dfb1c20
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-05-09 21:15:58 +0900

    Add / 걷기와 대기 시 애니메이션 추가

---
+++ Assets/Scripts/Entities/Controllers/TopDownAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TopDownAnimatorController : AnimationController
+{
+    private static readonly int _isWalking = Animator.StringToHash("isWalking");
+
+    private readonly float _magnituteThreshold = 0.5f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 obj)
+    {
+        animator.SetBool(_isWalking, obj.magnitude > _magnituteThreshold);
+    }
+}
