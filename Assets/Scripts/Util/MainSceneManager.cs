1a16a1c92a86842016b5365afd6ef6d3dabfb70e
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-05-13 17:08:40 +0900

    Fix / characterList 출력 수정

--- Assets/Scripts/Util/MainSceneManager.cs
+++ Assets/Scripts/Util/MainSceneManager.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainSceneManager :MonoBehaviour
+public class MainSceneManager : MonoBehaviour
 {
     public static MainSceneManager Instance;
     public GameObject CharacterList;
@@ -17,37 +17,50 @@ public class MainSceneManager :MonoBehaviour
     }
     private void Start()
     {
+        Debug.Log("Script started");
         // Player와 NPC 레이어의 객체들에서 텍스트를 수집
-        List<string> texts = CollectTextsFromLayers(new string[] { "Npc","Player" });
+        List<string> texts = CollectTextsFromLayers(new string[] { "Player", "Npc" });
+        Debug.Log("Texts collected: " + texts.Count);
 
         // 수집된 텍스트를 하나씩 출력
         foreach (string text in texts)
         {
             CharacterListText.text += text + "\n";
+            Debug.Log("Collected Text: " + text);
         }
     }
     List<string> CollectTextsFromLayers(string[] layers)
     {
         List<string> collectedTexts = new List<string>();
-        Text[] allTexts = FindObjectsOfType<Text>();
-
-        foreach (Text textComponent in allTexts)
+        foreach (string layer in layers)
         {
-            GameObject obj = textComponent.gameObject;
-            int objLayer = obj.layer;
-            foreach (string layer in layers)
+            int layerMask = LayerMask.GetMask(layer);
+            GameObject[] objects = FindObjectsOfType<GameObject>();
+
+            foreach (GameObject obj in objects)
             {
-                int layerMask = LayerMask.NameToLayer(layer);
-                if (objLayer == layerMask)
+                if (((1 << obj.layer) & layerMask) != 0)
                 {
-                    if (!collectedTexts.Contains(textComponent.text))
+                    // 최상위 GameObject에서 Text 컴포넌트를 찾기
+                    Text textComponent = GetRootGameObject(obj).GetComponentInChildren<Text>(true);
+                    if (textComponent != null && !collectedTexts.Contains(textComponent.text))
                     {
                         collectedTexts.Add(textComponent.text);
+                        Debug.Log($"Found text in {layer} layer: {textComponent.text}");
                     }
-                    break; 
                 }
             }
         }
         return collectedTexts;
+    }
+
+    GameObject GetRootGameObject(GameObject obj)
+    {
+        Transform current = obj.transform;
+        while (current.parent != null)
+        {
+            current = current.parent;
+        }
+        return current.gameObject;
     }
 }
